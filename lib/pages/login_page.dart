aa56d797ab8ec051fa90274b083a3c33c6dce4c2
Tales Rocha <taleshrochaz@gmail.com>
2024-10-28 11:38:10 -0300

    Ajustes loginPage (#15)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -23,9 +23,9 @@ class _LoginPageState extends State<LoginPage> {
     User? user = widget.userGroup.findByEmailAndPassword(email, password);
 
     if (user != null) {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => ObjectListPage(userGroup: widget.userGroup, loggedUser: user))
-      );
+      Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) =>
+              ObjectListPage(userGroup: widget.userGroup, loggedUser: user)));
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Email ou senha inválidos.')),
@@ -37,77 +37,80 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.connect_without_contact_rounded,
-                size: 148,
-                color: theme.colorScheme.primary,
-              ),
-              const SizedBox(height: 32.0),
-              Text(
-                "Seja bem vindo!",
-                style: theme.textTheme.headlineMedium,
-              ),
-              const SizedBox(height: 48.0),
-              TextField(
-                controller: emailController,
-                style: theme.textTheme.bodyMedium,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.crop_square,
+                  size: 148,
+                  color: theme.colorScheme.primary,
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-              const SizedBox(height: 16.0),
-              TextField(
-                controller: passwordController,
-                style: theme.textTheme.bodyMedium,
-                decoration: const InputDecoration(
-                  labelText: 'Senha',
+                const SizedBox(height: 32.0),
+                Text(
+                  "Seja bem vindo!",
+                  style: theme.textTheme.headlineMedium,
                 ),
-                obscureText: true,
-              ),
-              const SizedBox(height: 16.0),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: TextButton(
-                  onPressed: () {}, //problema seu que não lembra da senha
-                  child: const Text('Esqueceu a senha?'),
+                const SizedBox(height: 48.0),
+                TextField(
+                  controller: emailController,
+                  style: theme.textTheme.bodyMedium,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
                 ),
-              ),
-              const SizedBox(height: 32.0),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {
-                    handleLogin(emailController.text, passwordController.text);
-                  },
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(color: AppTheme.neutralLightest),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: passwordController,
+                  style: theme.textTheme.bodyMedium,
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                  ),
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {}, //problema seu que não lembra da senha
+                    child: const Text('Esqueceu a senha?'),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Expanded(
-                    child: Text("Não tem conta? "),
-                  ),
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {}, //tela de criação
-                      child: const Text('Crie uma aqui!'),
+                const SizedBox(height: 32.0),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      handleLogin(
+                          emailController.text, passwordController.text);
+                    },
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: AppTheme.neutralLightest),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Expanded(
+                      child: Text("Não tem conta? "),
+                    ),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: () {}, //tela de criação
+                        child: const Text('Crie uma aqui!'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
