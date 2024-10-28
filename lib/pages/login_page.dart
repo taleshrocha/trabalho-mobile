5f214da4315aa965b0bded5201327e1866b09042
Fernando Almeida <fernandogdalmeida@gmail.com>
2024-10-28 02:08:09 -0300

    tela de listagem de objetos completa

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho_mobile/entities/group.dart';
 import 'package:trabalho_mobile/entities/user.dart';
-import 'package:trabalho_mobile/logic/repository.dart';
 import 'package:trabalho_mobile/themes/theme.dart';
 import 'package:trabalho_mobile/pages/object_list_page.dart';
 
+import '../entities/person.dart';
+
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  final Group userGroup;
+
+  const LoginPage({super.key, required this.userGroup});
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -16,11 +20,11 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController passwordController = TextEditingController();
 
   void handleLogin(String email, String password) {
-    User? user = Repository.findByEmailAndPassword(email, password);
+    User? user = widget.userGroup.findByEmailAndPassword(email, password);
 
     if (user != null) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => ObjectListPage()),
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => ObjectListPage(userGroup: widget.userGroup, loggedUser: user))
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -33,72 +37,78 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              Icons.crop_square,
-              size: 148,
-              color: theme.colorScheme.primary,
-            ),
-            const SizedBox(height: 32.0),
-            Text(
-              "Seja bem vindo!",
-              style: theme.textTheme.headlineMedium,
-            ),
-            const SizedBox(height: 48.0),
-            TextField(
-              controller: emailController,
-              style: theme.textTheme.bodyMedium,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.connect_without_contact_rounded,
+                size: 148,
+                color: theme.colorScheme.primary,
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: passwordController,
-              style: theme.textTheme.bodyMedium,
-              decoration: const InputDecoration(
-                labelText: 'Senha',
+              const SizedBox(height: 32.0),
+              Text(
+                "Seja bem vindo!",
+                style: theme.textTheme.headlineMedium,
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: TextButton(
-                onPressed: () {},
-                child: const Text('Esqueceu a senha?'),
+              const SizedBox(height: 48.0),
+              TextField(
+                controller: emailController,
+                style: theme.textTheme.bodyMedium,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
               ),
-            ),
-            const SizedBox(height: 32.0),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  handleLogin(emailController.text, passwordController.text);
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: AppTheme.neutralLightest),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: passwordController,
+                style: theme.textTheme.bodyMedium,
+                decoration: const InputDecoration(
+                  labelText: 'Senha',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: TextButton(
+                  onPressed: () {}, //problema seu que não lembra da senha
+                  child: const Text('Esqueceu a senha?'),
                 ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Não tem conta? "),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Crie uma aqui!'),
+              const SizedBox(height: 32.0),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    handleLogin(emailController.text, passwordController.text);
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: AppTheme.neutralLightest),
+                  ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              const SizedBox(height: 20.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: Text("Não tem conta? "),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {}, //tela de criação
+                      child: const Text('Crie uma aqui!'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
