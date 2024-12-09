879e0f4ee2dea04ebebb9323bdfc6da38bc2811e
taleshrocha <taleshrochaz@gmail.com>
2024-12-08 23:26:36 -0300

    feat: uploading files

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:trabalho_mobile/logic/repository.dart';
 import 'package:trabalho_mobile/pages/api_test_page.dart';
+import 'package:trabalho_mobile/pages/file_test_page.dart';
 import 'package:trabalho_mobile/pages/login_page.dart';
 import 'package:trabalho_mobile/themes/theme.dart';
 import 'package:trabalho_mobile/providers/event_provider.dart';
+import 'package:trabalho_mobile/providers/file_provider.dart'; // Import your FileProvider
 import 'entities/group.dart';
 
 void main() {
@@ -21,15 +23,13 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         Provider<Group>.value(value: mainGroup),
-        ChangeNotifierProvider(
-          create: (context) => EventProvider(),
-        ),
+        ChangeNotifierProvider(create: (context) => EventProvider()),
       ],
       child: MaterialApp(
         title: 'Collectiva',
         theme: AppTheme.lightTheme,
         debugShowCheckedModeBanner: false,
-        home: ApiTestPage(userGroup: mainGroup),
+        home: FileTestPage(),
       ),
     );
   }
